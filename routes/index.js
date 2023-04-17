611a6660d978aa45886854fd8ff5eb6302f3377b
Xin Ning <ningxin1007@hotmail.com>
2023-04-16 20:38:05 -0400

    Test root

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: `Express`, content:`App listening on port ${process.env.PORT || '3000'}` });
+  // res.render('index', { title: `Express`, content:`App listening on port ${process.env.PORT || '3000'}` });
+  res.json({message:`App listening on port ${process.env.PORT || '3000'}`})
 });
 
 module.exports = router;
